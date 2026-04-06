6ac4d88cc8f9a8acd3cb4ea5fa090983562503f5
Kimmy <clarkkimberly415@gmail.com>
2026-04-05 23:29:44 -0400

    final path fix !

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/moore-love-care-test/', // Leading and trailing slashes are REQUIRED
+  base: '/moore-love-care-test/', // L eading and trailing slashes are REQUIRED
 })
