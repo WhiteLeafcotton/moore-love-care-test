1f82297d1bcef829c6ec0b07c802344048e4b59d
Kimmy <clarkkimberly415@gmail.com>
2026-04-05 21:17:45 -0400

    final

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/moore-love-care-test/', // <--- ADD YOUR REPO NAME HERE
+  base: '/moore-love-care-test/', // Leading and trailing slashes are REQUIRED
 })
