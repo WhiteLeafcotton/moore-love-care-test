c5f4f27d22d3249afa9ddfa31b7924d6fa658655
Kimmy <clarkkimberly415@gmail.com>
2026-04-07 21:01:49 -0400

    Refactor Scene component to increase platform height to 3.5 for improved alignment under window edges

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,6 +1,6 @@
 import { useRef, useMemo } from "react";
 import { useThree, useFrame, extend, useLoader } from "@react-three/fiber";
-import { Environment, Sky, ContactShadows } from "@react-three/drei";
+import { Environment, Sky } from "@react-three/drei";
 import { Water } from "three-stdlib";
 import * as THREE from "three";
 
@@ -62,9 +62,9 @@ export default function Scene({ currentView }) {
       <Environment preset="dawn" />
       
       <group position={[0, 0, 0]}>
-        {/* PLATFORM: Height increased to 2.5 to sit under window edges */}
-        <mesh receiveShadow position={[12, -0.25, 15]}>
-          <boxGeometry args={[9, 2.5, 28]} />
+        {/* PLATFORM: Height increased to 3.5 */}
+        <mesh receiveShadow position={[12, -0.75, 15]}>
+          <boxGeometry args={[9, 3.5, 28]} />
           <meshStandardMaterial map={travertineTex} color="#f1dfd8" />
         </mesh>
 
