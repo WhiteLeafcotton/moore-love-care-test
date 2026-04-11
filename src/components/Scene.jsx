c8613d530e134e8345142cba43230c1db443db59
Kimmy <clarkkimberly415@gmail.com>
2026-04-11 15:03:29 -0400

    gygfrtfg

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -227,13 +227,13 @@ export default function Scene({ currentView }) {
       </group>
 
       <group position={[0, 0, 0]}>
-        {/* PLATFORM nudged back slightly to align with forward stairs */}
-        <mesh position={[12.5, -2.0, 16.5]} castShadow receiveShadow>
+        {/* PLATFORM nudged further to match the new stair Z-index */}
+        <mesh position={[12.5, -2.0, 19.5]} castShadow receiveShadow>
           <boxGeometry args={[14, 8.0, 28]} /><meshStandardMaterial {...pinkProps} />
         </mesh>
         
-        {/* POSITION FIX: Staircase moved forward (+Z) to clear the wall boundary */}
-        <Staircase position={[5.0, 1.5, 2.5]} rotation={[0, -Math.PI / 2, 0]} width={17.5} texture={pinkStoneTex} />
+        {/* PUSHED FURTHER: Staircase moved significantly forward (+Z) to clear all wall clipping */}
+        <Staircase position={[5.0, 1.5, 5.5]} rotation={[0, -Math.PI / 2, 0]} width={17.5} texture={pinkStoneTex} />
         
         <group position={[-16, -1, 0]}>
           <mesh position={[1, 8.5, 0]} castShadow receiveShadow><boxGeometry args={[4, 17, 2]} /><meshStandardMaterial {...pinkProps} /></mesh>
