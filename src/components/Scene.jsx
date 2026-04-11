7415933ea65a9a0249b3cde80307349cfd54ebec
Kimmy <clarkkimberly415@gmail.com>
2026-04-11 19:41:49 -0400

    tesrdfgv

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -285,8 +285,8 @@ export default function Scene({ currentView }) {
             <BlockHumanoid scale={0.9} materialProps={butterProps} poseProps={{ leftLegRotation: [-0.3, 0, 0], rightLegRotation: [0.3, 0, 0], position: [0.4, 0, -0.1]}} />
           </group>
 
-          {/* Couple A: MOVED FORWARD TOWARD CAMERA (Higher Z, Lower X) */}
-          <group position={[8.5, 1.1, 13.5]} rotation={[0, Math.PI / 2, 0]}>
+          {/* Couple A: AGGRESSIVELY FORWARD TOWARD CAMERA (Higher Z, Lower X) */}
+          <group position={[4.5, 0.4, 21.5]} rotation={[0, Math.PI / 2, 0]}>
             <BlockHumanoid scale={0.9} materialProps={butterProps} poseProps={{ leftLegRotation: [Math.PI / 2, 0, 0], rightLegRotation: [Math.PI / 2, 0, 0], position: [-0.2, 0, 0]}} />
             <BlockHumanoid scale={0.88} materialProps={butterProps} poseProps={{ leftLegRotation: [Math.PI / 2, 0, 0], rightLegRotation: [Math.PI / 2, 0, 0], position: [0.5, 0, 0]}} />
           </group>
