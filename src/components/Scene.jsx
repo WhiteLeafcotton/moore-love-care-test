501ada5b190fa896c3287e7596bf28aba2d87983
Kimmy <clarkkimberly415@gmail.com>
2026-04-11 20:16:27 -0400

    huhuhu

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -285,8 +285,8 @@ export default function Scene({ currentView }) {
             <BlockHumanoid scale={0.9} materialProps={butterProps} poseProps={{ leftLegRotation: [-0.3, 0, 0], rightLegRotation: [0.3, 0, 0], position: [0.4, 0, -0.1]}} />
           </group>
 
-          {/* Couple A: MOVED LEFT (Higher X) while staying on the correct step height */}
-          <group position={[8.5, -0.6, 26.5]} rotation={[0, Math.PI / 2, 0]}>
+          {/* Couple A: Adjusted position to sit properly on steps and shifted left */}
+          <group position={[11.5, -0.6, 26.5]} rotation={[0, Math.PI / 2, 0]}>
             <BlockHumanoid scale={0.9} materialProps={butterProps} poseProps={{ leftLegRotation: [Math.PI / 2, 0, 0], rightLegRotation: [Math.PI / 2, 0, 0], position: [-0.2, 0, 0]}} />
             <BlockHumanoid scale={0.88} materialProps={butterProps} poseProps={{ leftLegRotation: [Math.PI / 2, 0, 0], rightLegRotation: [Math.PI / 2, 0, 0], position: [0.5, 0, 0]}} />
           </group>
