14b27e5dfca9d8b6af68189a59b5c6176cf7a11f
Kimmy <clarkkimberly415@gmail.com>
2026-04-15 00:05:02 -0400

    HUHGYGYG

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -88,46 +88,50 @@ const HomeLamp = ({ position }) => (
 
 // --- THE CIRCULAR FLOATING PLATFORM (SANCUTARY) ---
 // --- THE CIRCULAR FLOATING PLATFORM (SANCUTARY) ---
+// --- THE CIRCULAR FLOATING PLATFORM (SANCUTARY) ---
 const FloatingPlatform = ({ butterProps }) => {
   return (
-    <Float speed={1.8} rotationIntensity={0.15} floatIntensity={0.4} position={[11, -1.0, 17]}>
-      {/* Platform Disk - 0.25 Height */}
-      <mesh renderOrder={10000} frustumCulled={false}>
+    <Float speed={1.8} rotationIntensity={0.2} floatIntensity={0.5} position={[11, -1.0, 17]}>
+      {/* Platform Disk */}
+      <mesh renderOrder={10000}>
         <cylinderGeometry args={[4.2, 4.2, 0.25, 64]} />
         <meshBasicMaterial color="#ffffff" depthTest={false} transparent opacity={0.8} />
       </mesh>
 
-      {/* Circle Rug */}
+      {/* Circle Rug under Chair */}
       <mesh position={[-0.8, 0.13, 0]} rotation={[-Math.PI / 2, 0, 0]} renderOrder={10001}>
-        <circleGeometry args={[2.8, 64]} />
-        <meshBasicMaterial color={TITLE_PURPLE} depthTest={false} transparent opacity={0.2} />
+        <circleGeometry args={[2.2, 64]} />
+        <meshBasicMaterial color={TITLE_PURPLE} depthTest={false} transparent opacity={0.3} />
       </mesh>
 
-      {/* Chair (rotated 180 degrees) & Bob */}
-      <group position={[-0.8, 0.15, 0]} rotation={[0, Math.PI, 0]}>
-        <LazyBoyChair position={[0, 0, 0]} />
-        <group position={[0, 0.47, 0]}>
-          <BlockHumanoid 
-            scale={0.8} 
-            materialProps={{...butterProps, depthTest: false}} 
-            poseProps={{ 
-              leftLegRotation: [1.5, 0, 0], 
-              rightLegRotation: [1.5, 0, 0], 
-              torsoRotationX: 0.05 
-            }} 
-          />
-        </group>
+      {/* --- LARGER ELEMENTS --- */}
+
+      {/* The Recliner - SCALE INCREASED from 0.7 to 0.9 */}
+      <LazyBoyChair position={[-0.8, 0.15, 0]} rotation={[0, Math.PI / 4, 0]} scale={0.9} />
+
+      {/* Seated Resident (Bob) - SCALE INCREASED from 0.8 to 1.0 */}
+      <group position={[-0.8, 0.62, 0]} rotation={[0, Math.PI / 4, 0]}>
+        <BlockHumanoid 
+          scale={1.0} 
+          materialProps={{...butterProps, depthTest: false}} 
+          poseProps={{ 
+            leftLegRotation: [1.5, 0, 0], 
+            rightLegRotation: [1.5, 0, 0], 
+            torsoRotationX: 0.05 
+          }} 
+        />
       </group>
 
-      {/* Home Lamp over Bob's head */}
-      <HomeLamp position={[-2.2, 0.14, 0]} />
+      {/* Home Lamp - SCALE INCREASED from 1.0 to 1.2 */}
+      {/* (Assuming HomeLamp is defined elsewhere) */}
+      <HomeLamp position={[-2.2, 0.14, 0]} scale={1.2} />
 
-      {/* FIXED: Helper now pulled inward toward the center and raised to sit on the surface */}
-      {/* Position adjusted from [1.4, 0.14, 1.1] to [0.8, 0.25, 0.8] */}
-      <group position={[0.6, 0.27, 0.6]} rotation={[0, -Math.PI / 1.5, 0]} renderOrder={10002}>
+      {/* Helper (Standing on Platform) - SCALE INCREASED from 0.92 to 1.15 */}
+      {/* Y-position adjusted slightly higher to sit on platform due to scale */}
+      <group position={[1.2, 0.2, 0.9]} rotation={[0, -Math.PI / 1.5, 0]}>
         <BlockHumanoid 
           isHelper 
-          scale={0.92} 
+          scale={1.15} 
           materialProps={{...butterProps, depthTest: false}} 
           poseProps={{ 
             headRotationY: -0.4, 
